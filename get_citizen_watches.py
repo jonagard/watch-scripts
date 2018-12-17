e1b37fc0d1c4d13764a5b20f7388e6a17add7617
Jonathan Gardner <jonagard@gmail.com>
2018-12-16 21:10:49 -0500

    First commit
    Citizen and Seiko

---
+++ get_citizen_watches.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+preurl = "https://www.citizenwatch.com/us/en/{}/?sz=24&start={}&format=page-element"
+sex = 'mens'
+
+start = 0
+url = preurl.format(sex, start)
+
+while True:
+    page = requests.get(url)
+    soup = bs(page.text, 'html.parser')
+    watch_tile = soup.find_all(class_='product-tile')
+    if not watch_tile:
+        break
+    for item in watch_tile:
+        filename = item.get('data-itemid')
+        img = item.find_all('img')
+        try:
+            response = requests.get('http:{}'.format(img[0].get('src')))
+            with open('C:\\Users\\jonnyg\\Desktop\\watches\\{}.png'.format(filename), 'wb') as f:
+                f.write(response.content)
+        except:
+            print('Failed to get image for {}'.format(filename))
+
+    start = start + 24
+    url = preurl.format(sex, start)
---
+++ get_seikio_watches.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+# 2 = men
+# 1 = women
+sex = 1
+
+# The unit field just has to be larger than how many total watches they have.
+# At the time of this writing, they had 191 men's watches
+page = requests.get('https://www.seikowatches.com/__api/posts/list?category_id=11137&custom_field__sex_web_search={}&paginate=true&unit=200'.format(sex))
+watches = page.json()
+
+for item in watches['results']:
+    filename = item['thumbnail']['file_name']
+    url = 'https://storage.seikowatches.com/production/images' + item['thumbnail']['url_key'] + '_medium.png'
+    try:
+        response = requests.get(url)
+        with open('C:\\Users\\jonnyg\\Desktop\\watches\\{}.png'.format(filename), 'wb') as f:
+            f.write(response.content)
+    except:
+        print('Failed to get image for {}'.format(filename))
